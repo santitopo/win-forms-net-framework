429e278bc4f08bc7fef95ff8bc5b859740fe96c8
santitopo <santiagotopolansky@gmail.com>
2020-04-22 16:27:38 -0300

    Adding class Entity

---
+++ AnalisisSentimientos/BusinessLogic/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Entity
+    {
+        public String Name { get; set; }
+        public Entity(String name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ AnalisisSentimientos/Tests/EntityTest.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class EntityTest
+    {
+        [TestMethod]
+        public void NewEntity()
+        {
+            String name = "James";
+            Entity ent = new Entity(name);
+           
+            Assert.AreEqual(ent.Name, name);
+        }
+    }
+}
