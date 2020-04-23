fb35bc9180e5ecfbfb1f8a65275f7f2258122470
santitopo <santiagotopolansky@gmail.com>
2020-04-22 23:34:10 -0300

    Empty Constructor, Equals and ToString (Entity)

--- AnalisisSentimientos/BusinessLogic/Entity.cs
+++ AnalisisSentimientos/BusinessLogic/Entity.cs
@@ -5,9 +5,34 @@ namespace BusinessLogic
     public class Entity
     {
         public String Name { get; set; }
+
+        public Entity()
+        {
+
+        }
         public Entity(String name)
         {
             Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // If the passed object is null
+            if (obj == null)
+            {
+                return false;
+            }
+            // If the passed object is not an entity
+            if (!(obj is Entity))
+            {
+                return false;
+            }
+            return (Name == ((Entity)obj).Name));
+        }
+
+        public override String ToString()
+        {
+            return Name;
         }
     }
 }
