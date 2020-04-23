a1125b6d0aaa8ec6281f7799deb6a0e6ec4e2599
santitopo <santiagotopolansky@gmail.com>
2020-04-23 13:29:09 -0300

    Preliminar EntityClass Finished (Entity)

--- AnalisisSentimientos/Tests/EntityTest.cs
+++ AnalisisSentimientos/Tests/EntityTest.cs
@@ -12,8 +12,48 @@ namespace Tests
         {
             String name = "James";
             Entity ent = new Entity(name);
-           
+
             Assert.AreEqual(ent.Name, name);
         }
+
+        [TestMethod]
+        public void EqualEnties()
+        {
+            Entity e1 = new Entity();
+            e1.Name = "Coca";
+            Entity e2 = new Entity();
+            e2.Name = "Coca";
+            Assert.IsTrue(e1.Equals(e2));
+        }
+
+        [TestMethod]
+
+        public void DifferentEntities()
+        {
+            Entity e1 = new Entity();
+            Entity e2 = new Entity();
+            e1.Name = "Coca";
+            e2.Name = "Sprite";
+            Assert.IsFalse(e1.Equals(e2));
+        }
+
+        [TestMethod]
+
+        public void NullCompare()
+        {
+            Entity e1 = new Entity();
+            e1.Name = "Coca";
+            Assert.IsFalse(e1.Equals(null));
+        }
+
+        [TestMethod]
+
+        public void EntitytoString()
+        {
+            Entity e1 = new Entity();
+            e1.Name = "Coca";
+            Assert.AreEqual(e1.ToString(),"Coca");
+        }
+
     }
 }
