845a422075914b6822c79bddf6016f6b39739a22
Felipe <felipe>
2020-04-22 23:07:49 -0300

    ClassFeelingFinished (Feeling)

---
+++ AnalisisSentimientos/Tests/FeelingTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class FeelingTests
+    {
+        Feeling f;
+
+        [TestInitialize]
+        public void SetUp(){
+            f = new Feeling();
+        }
+
+        [TestMethod]
+        public void newFeelingFstParameter()
+        {
+            Feeling f2 = new Feeling("Excelente", true);
+            Assert.AreEqual("Excelente", f2.Name);
+        }
+
+        [TestMethod]
+        public void newFeelingSndParameter()
+        {
+            Feeling f2 = new Feeling("Excelente", true);
+            Assert.IsTrue(f2.Type);
+        }
+
+        [TestMethod]
+        public void equals()
+        {
+            f.Type = true;
+            f.Name = "Bueno";
+
+            Feeling f2 = new Feeling();
+            f2.Name = "Bueno";
+            f2.Type = true;
+
+            Assert.IsTrue(f.Equals(f2));
+        }
+
+        [TestMethod]
+        public void equalsNull()
+        {
+            f.Type = true;
+            f.Name = "Bueno";
+
+            Feeling f2 = null;
+
+            Assert.IsFalse(f.Equals(f2));
+        }
+
+        [TestMethod]
+        public void notEquals()
+        {
+            f.Type = true;
+            f.Name = "Bueno";
+
+            Feeling f2 = new Feeling();
+            f2.Name = "Malo";
+            f2.Type = true;
+
+            Assert.IsFalse(f.Equals(f2));
+        }
+
+        [TestMethod]
+        public void toString()
+        {
+            f.Type = true;
+            f.Name = "Bueno";
+
+            string expectedOutput = "Name:Bueno, Type:True";
+
+            Assert.AreEqual(f.ToString(), expectedOutput);
+        }
+
+
+    }
+}
