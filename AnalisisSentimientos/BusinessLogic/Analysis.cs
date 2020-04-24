4f530ea5cfec81d53b02ea4591b94aafaac92bdf
santitopo <santiagotopolansky@gmail.com>
2020-04-24 10:48:48 -0300

    First TestCases Added (Analysis)

---
+++ AnalisisSentimientos/BusinessLogic/Analysis.cs
@@ -0,0 +1,15 @@
+﻿namespace BusinessLogic
+{
+    public class Analysis
+    {
+        public Entity Entity { get; set; }
+        public Phrase Phrase { get; set; }
+        public enum Type
+        {
+            positive,
+            negative,
+            neutral
+        }
+        public Type PhraseType { get; set; }
+    }
+}
---
+++ AnalisisSentimientos/Tests/AnalysisTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnalysisTest
+    {
+        [TestMethod]
+        public void newAnalysis()
+        {
+            Phrase p = new Phrase();
+            Entity e = new Entity("Coca");
+            Analysis a = new Analysis()
+            {
+                Phrase = p,
+                PhraseType = Analysis.Type.positive,
+                Entity = e
+
+            };
+            Assert.AreEqual(a.Phrase, p);
+            Assert.AreEqual(a.PhraseType, Analysis.Type.positive);
+            Assert.AreEqual(a.Entity, e);
+        }
+    }
+}
