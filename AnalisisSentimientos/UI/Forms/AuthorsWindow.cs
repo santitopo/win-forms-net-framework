5d9ed915a1919ec9feefdde6705fd7248d27b668
santitopo <santiagotopolansky@gmail.com>
2020-06-08 17:27:05 -0300

    Window Created

---
+++ AnalisisSentimientos/UI/Forms/AuthorsWindow.cs
@@ -0,0 +1,35 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Forms
+{
+    public partial class AuthorsWindow : Form
+    {
+        private AuthorLogic subsystemAuthors;
+        public AuthorsWindow(AuthorLogic a)
+        {
+            InitializeComponent();
+            subsystemAuthors = a;
+            RefreshAuthors();
+            //try
+            //{
+            //    datagridAuthors.Columns["TimeBack"].HeaderText = "Time (Hours)";
+            //}
+            //catch (Exception) { }
+        }
+
+        public void RefreshAuthors()
+        {
+            
+            datagridAuthors.DataSource = subsystemAuthors.ListByEntityNumberDesc();
+        }
+    }
+}
