8c6532f222e9deb47d62d46bc4eb3a0b69f40f1f
santitopo <santiagotopolansky@gmail.com>
2020-06-09 12:15:06 -0300

    UI component design for Author Displays

--- AnalisisSentimientos/UI/Forms/AuthorsWindow.Designer.cs
+++ AnalisisSentimientos/UI/Forms/AuthorsWindow.Designer.cs
@@ -30,16 +30,26 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.datagridAuthors = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtnEntities = new System.Windows.Forms.RadioButton();
+            this.rbtnRatio = new System.Windows.Forms.RadioButton();
+            this.rbtnAverage = new System.Windows.Forms.RadioButton();
+            this.cmbphraseType = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.datagridAuthors)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.datagridAuthors);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1082, 556);
+            this.panel1.Size = new System.Drawing.Size(1098, 695);
             this.panel1.TabIndex = 0;
             // 
             // datagridAuthors
@@ -48,24 +58,117 @@
             this.datagridAuthors.AllowUserToDeleteRows = false;
             this.datagridAuthors.AllowUserToOrderColumns = true;
             this.datagridAuthors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagridAuthors.Location = new System.Drawing.Point(12, 36);
+            this.datagridAuthors.Location = new System.Drawing.Point(39, 191);
+            this.datagridAuthors.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.datagridAuthors.Name = "datagridAuthors";
             this.datagridAuthors.ReadOnly = true;
             this.datagridAuthors.RowHeadersWidth = 51;
             this.datagridAuthors.RowTemplate.Height = 24;
-            this.datagridAuthors.Size = new System.Drawing.Size(1035, 493);
+            this.datagridAuthors.Size = new System.Drawing.Size(999, 472);
             this.datagridAuthors.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(481, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 33);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Listar Segun:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.Controls.Add(this.cmbphraseType);
+            this.groupBox2.Controls.Add(this.rbtnAverage);
+            this.groupBox2.Controls.Add(this.rbtnEntities);
+            this.groupBox2.Controls.Add(this.rbtnRatio);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox2.Location = new System.Drawing.Point(12, 46);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(1058, 125);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Criterios";
+            // 
+            // rbtnEntities
+            // 
+            this.rbtnEntities.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbtnEntities.AutoSize = true;
+            this.rbtnEntities.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnEntities.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.rbtnEntities.Location = new System.Drawing.Point(191, 70);
+            this.rbtnEntities.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.rbtnEntities.Name = "rbtnEntities";
+            this.rbtnEntities.Size = new System.Drawing.Size(350, 37);
+            this.rbtnEntities.TabIndex = 2;
+            this.rbtnEntities.Text = "Entidades Mencionadas";
+            this.rbtnEntities.UseVisualStyleBackColor = true;
+            this.rbtnEntities.Click += new System.EventHandler(this.rbtnEntitiesChecked);
+            // 
+            // rbtnRatio
+            // 
+            this.rbtnRatio.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbtnRatio.AutoSize = true;
+            this.rbtnRatio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnRatio.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.rbtnRatio.Location = new System.Drawing.Point(191, 25);
+            this.rbtnRatio.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.rbtnRatio.Name = "rbtnRatio";
+            this.rbtnRatio.Size = new System.Drawing.Size(353, 37);
+            this.rbtnRatio.TabIndex = 3;
+            this.rbtnRatio.Text = "Porcentaje Comentarios";
+            this.rbtnRatio.UseVisualStyleBackColor = true;
+            this.rbtnRatio.Click += new System.EventHandler(this.rbtnRatio_CheckedChanged);
+            // 
+            // rbtnAverage
+            // 
+            this.rbtnAverage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbtnAverage.AutoSize = true;
+            this.rbtnAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnAverage.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.rbtnAverage.Location = new System.Drawing.Point(561, 70);
+            this.rbtnAverage.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.rbtnAverage.Name = "rbtnAverage";
+            this.rbtnAverage.Size = new System.Drawing.Size(348, 37);
+            this.rbtnAverage.TabIndex = 4;
+            this.rbtnAverage.Text = "Promedio Frases Diario";
+            this.rbtnAverage.UseVisualStyleBackColor = true;
+            this.rbtnAverage.Click += new System.EventHandler(this.rbtnAverage_CheckedChanged);
+            // 
+            // cmbphraseType
+            // 
+            this.cmbphraseType.Enabled = false;
+            this.cmbphraseType.FormattingEnabled = true;
+            this.cmbphraseType.Items.AddRange(new object[] {
+            "Positivos",
+            "Negativos"});
+            this.cmbphraseType.Location = new System.Drawing.Point(561, 21);
+            this.cmbphraseType.Name = "cmbphraseType";
+            this.cmbphraseType.Size = new System.Drawing.Size(348, 41);
+            this.cmbphraseType.TabIndex = 5;
+            this.cmbphraseType.SelectedValueChanged += new System.EventHandler(this.rbtnRatio_CheckedChanged);
             // 
             // AuthorsWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 553);
+            this.ClientSize = new System.Drawing.Size(1100, 691);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AuthorsWindow";
             this.Text = "AuthorsWindow";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.datagridAuthors)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -74,5 +177,11 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView datagridAuthors;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtnEntities;
+        private System.Windows.Forms.RadioButton rbtnRatio;
+        private System.Windows.Forms.ComboBox cmbphraseType;
+        private System.Windows.Forms.RadioButton rbtnAverage;
     }
 }
--- AnalisisSentimientos/UI/Forms/AuthorsWindow.cs
+++ AnalisisSentimientos/UI/Forms/AuthorsWindow.cs
@@ -18,18 +18,62 @@ namespace UI.Forms
         {
             InitializeComponent();
             subsystemAuthors = a;
+            cmbphraseType.SelectedIndex = 0;
             RefreshAuthors();
-            //try
-            //{
-            //    datagridAuthors.Columns["TimeBack"].HeaderText = "Time (Hours)";
-            //}
-            //catch (Exception) { }
+            try
+            {
+                displayWantedColumns();
+            }
+            catch (Exception) { }
         }
 
+        private void displayWantedColumns()
+        {
+            for (int i = 0; i < datagridAuthors.Columns.Count; i++)
+            {
+                datagridAuthors.Columns[i].Visible = false;
+            }
+            datagridAuthors.Columns["Username"].Visible = true;
+            datagridAuthors.Columns["Name"].Visible = true;
+            datagridAuthors.Columns["Surname"].Visible = true;
+        }
         public void RefreshAuthors()
         {
-            
+            datagridAuthors.DataSource = subsystemAuthors.GetAuthors;
+        }
+
+       private void rbtnEntitiesChecked(object sender, EventArgs e)
+        {
+            cmbphraseType.Enabled = false;
+            datagridAuthors.DataSource = null;
             datagridAuthors.DataSource = subsystemAuthors.ListByEntityNumberDesc();
+            displayWantedColumns();
+
+        }
+
+        private void rbtnAverage_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbphraseType.Enabled = false;
+            datagridAuthors.DataSource = null;
+            datagridAuthors.DataSource = subsystemAuthors.ListByPhraseAverageDesc();
+            displayWantedColumns();
+        }
+
+        private void rbtnRatio_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbphraseType.Enabled = true;
+            if (cmbphraseType.SelectedIndex!=-1 && cmbphraseType.SelectedItem.ToString().Equals("Positivos"))
+            {
+                datagridAuthors.DataSource = null;
+                datagridAuthors.DataSource = subsystemAuthors.ListByPositiveRatioDesc();
+                displayWantedColumns();
+            }
+            else
+            {
+                datagridAuthors.DataSource = null;
+                datagridAuthors.DataSource = subsystemAuthors.ListByNegativeRatioDesc();
+                displayWantedColumns();
+            }
         }
     }
 }
